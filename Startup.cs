59ccad8a5b6b8a7c167775d42c4340fd202141c8
mohamedSamyAli <msamy2019@gmail.com>
2019-04-25 11:37:36 +0200

    'first'

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(linkedIn.Startup))]
+namespace linkedIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
